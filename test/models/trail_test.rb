bd08a11f88ac3a2ca928b0a42cb9b522ef0af472
Rob Ring <rjm.ring@gmail.com>
2016-04-13 10:25:57 -0500

    Added unit tests for Trail

--- test/models/trail_test.rb
+++ test/models/trail_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class TrailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @caddy_lake_trail = trails(:caddy_lake)    
+  end
+
+  test "should create trail with unique_id" do
+    new_trail = Trail.new(name: 'West Hawk Lake', unique_id: '101')
+    assert new_trail.save  
+  end
+
+  test "should not create a trail with a duplicate unique_id" do
+    new_trail = Trail.new(name: 'West Hawk Lake', unique_id: '1')
+    assert_not new_trail.save
+  end
+
 end
